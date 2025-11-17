6b7cf7fbe08692bfcc13c3dc9b0a4ad540a632c9
baoxiong840 <bxiong44@wisc.edu>
2025-11-16 22:16:28 -0600

    Completed a successful creation and test of using a header.html file and inputting it into a test.html file

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("our website's javascript file!");
+
+// Header
+fetch('header.html')
+    .then(response => response.text())
+    .then(html => document.getElementById("header").innerHTML = html);
